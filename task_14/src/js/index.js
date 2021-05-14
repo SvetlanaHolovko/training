82e695f17929b715e3fb4e8c262d67cd35040279
SvetlanaHolovko <svetta.gsn@gmail.com>
2021-05-14 21:17:57 +0300

    Added block clients

--- task_14/src/js/index.js
+++ task_14/src/js/index.js
@@ -3,3 +3,45 @@ import Shady from './components/shady';
 const slimShady = new Shady('Slim');
 console.log(slimShady.fullName);
 
+$(function () {
+    
+    function initStars() {
+        const stars = $('.stars li');
+
+        stars.on('click', function () {
+            $(stars).removeClass('active');
+            $(this).toggleClass('active');
+
+        })
+    }
+
+    initStars();
+
+    /* Slider (Слайдер) */
+    $("[data-slider]").slick({
+        arrows: false, 
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 1000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow:2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow:1
+                }
+            }
+        ]
+    });
+
+    $('.slider').slick();
+    
+});
+
