d0adea89c2c0b9d416de4963fa0cf84939b9d3d4
SvetlanaHolovko <svetta.gsn@gmail.com>
2021-06-06 09:57:08 +0300

    Updates menu-burger

--- task_14/src/js/index.js
+++ task_14/src/js/index.js
@@ -1,18 +1,36 @@
-import Shady from './components/shady';
+// import Shady from './components/shady';
 
-const slimShady = new Shady('Slim');
-console.log(slimShady.fullName);
+// const slimShady = new Shady('Slim');
+// console.log(slimShady.fullName);
 
 $(function () {
+
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
     
-    function initStars() {
-        const stars = $('.stars li');
+    /* Прокрутка при клике*/
+    const menuLinks = document.querySelectorAll('.header__nav-link[data-scroll]');
 
-        stars.on('click', function () {
-            $(stars).removeClass('active');
-            $(this).toggleClass('active');
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener("click", onMenuLinkClick);
+        });
 
-        })
+        function onMenuLinkClick(e) {
+            e.preventDefault();
+            const menuLink = e.target;
+            if (menuLink.dataset.scroll && document.querySelector(menuLink.dataset.scroll)) {
+                const scrollBlock = document.querySelector(menuLink.dataset.scroll);
+                const scrollBlockValue = scrollBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+                window.scrollTo({
+                    top: scrollBlockValue,
+                    behavior: "smooth"
+                });
+            }
+        }
     }
 
     /* Slider (Слайдер) */
@@ -38,6 +56,16 @@ $(function () {
             }
         ]
     });
+
+    function initStars() {
+        const stars = $('.stars li');
+
+        stars.on('click', function () {
+            $(stars).removeClass('active');
+            $(this).toggleClass('active');
+
+        })
+    }
 
     initStars();
     
