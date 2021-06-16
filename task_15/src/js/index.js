4201d40d592e7704e1681fb0c30955d98f84dff5
SvetlanaHolovko <svetta.gsn@gmail.com>
2021-06-16 16:58:02 +0300

    Updates task_15

--- task_15/src/js/index.js
+++ task_15/src/js/index.js
@@ -1,45 +1,6 @@
 import Shady from './components/shady';
 
-const slimShady = new Shady('Slim');
-console.log(slimShady.fullName);
+// const slimShady = new Shady('Slim');
+// console.log(slimShady.fullName);
 
-$(function () {
-    
-    function initStars() {
-        const stars = $('.stars li');
-
-        stars.on('click', function () {
-            $(stars).removeClass('active');
-            $(this).toggleClass('active');
-
-        })
-    }
-
-    /* Slider (Слайдер) */
-    $("[data-slider]").slick({
-        arrows: false, 
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        speed: 1000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow:2
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow:1
-                }
-            }
-        ]
-    });
-
-    initStars();
-    
-});
 
